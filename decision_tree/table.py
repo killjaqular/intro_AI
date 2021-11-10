81190000d8ae7e8f7be085e48d0648434077ce19
Kippiii <ianorzel@gmail.com>
2021-11-10 18:47:28 -0500

    Making create_leaf and create_attribute_node static

--- decision_tree/table.py
+++ decision_tree/table.py
@@ -2,6 +2,15 @@
 A row of the table, representing a particular example
 """
 class TableRow():
+    """
+    Creates a new TableRow object (Should be private)
+
+    :param dict: Dictionary that maps an attribute to its value
+    :return: A new TableRow
+    """
+    def __init__(self, dict={}):
+        self.dict = dict
+
     pass
 
 """
