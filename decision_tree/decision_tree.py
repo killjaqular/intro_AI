a6211ab8a47b5f14436524ee5e4f5c0228ce387a
Kippiii <ianorzel@gmail.com>
2021-11-11 20:51:23 -0500

    Putting in the correct decision tree file

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,11 +1,72 @@
 class DecisionTree:
 
+    """
+    Creates a basic DecisionTree instance. Should be private
+
+    :param leaf: Whether or not the node is a leaf node
+    :param bool: If it is a leaf node, has its boolean value
+    :param children: A dictionary that maps the attribute value to a subtree
+    :param attribute: A string of the attribute name
+    :return: A DecisionTree object with these parameters set
+    """
     def __init__(self, leaf=False, bool=False, children={}, attribute=""):
         self.leaf = leaf
         self.bool = bool
         self.children = children
         self.attriute = attribute
 
-    def create_leaf(self, bool):
+    """
+    Creates a DecisionTree for a leaf node
+
+    :param bool: The boolean value of the leaf node
+    :return: A DecisionTree object
+    """
+    @staticmethod
+    def create_leaf(bool):
         return DecisionTree(leaf=True, bool=bool)
-    #TODO
+
+    """
+    Creates a DecisionTree for an attribute node
+
+    :param attribute: The string representing what attribute the node is related to
+    :param children: A dictionary that maps string attribute values to other trees
+    :return: A DecisionTree object
+    """
+    @staticmethod
+    def create_attribute_node(attribute, children):
+        return DicisionTree(attribute=attribute, children=children)
+
+    """
+    Determines whether the current node is a leaf
+
+    :return: A boolean thats true if the node is a leaf
+    """
+    def is_leaf(self):
+        return self.leaf
+
+    """
+    Returns the bool value of a leaf node
+
+    :return: The boolean value of a leaf node
+    """
+    def get_bool(self):
+        assert self.is_leaf()
+        return self.bool
+
+    """
+    Returns the attribute of an attribute node
+
+    :return: String of the attribute
+    """
+    def get_attribute(self):
+        assert not self.is_leaf()
+        return self.attribute
+
+    """
+    Returns the children of this node
+
+    :return: a dictionary that links attribute strings to trees
+    """
+    def get_children(self):
+        assert not self.is_leaf()
+        return self.children
