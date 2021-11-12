79b0ae9db1288b87bf110c68107bb0411ee4161c
Kippiii <ianorzel@gmail.com>
2021-11-12 12:37:09 -0500

    Creating the file for getting the input

---
+++ decision_tree/get_input.py
@@ -0,0 +1,8 @@
+"""
+Converts input from a tab-seperated value to a Table
+
+:param file_name: Name of the tab-seperated file
+:return: A Table object containing all of the information
+"""
+def get_input(file_name):
+    pass
